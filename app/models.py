2fdf964a208eaedee247c18e7535ec2fa0dafa37
Aaron <rscaposer@gmail.com>
2016-11-22 18:44:57 -0500

    models getting better

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(Base):
     name = Column(String(50), unique=True)
     email = Column(String(120), unique=True)
     password = Column(String(120))
+    #book = relationship("Book", secondary=BookUser, back_ref='User')
 
     def __init__(self, name=None, email=None, password=None):
         self.name = name
@@ -16,34 +17,32 @@ class User(Base):
 
     def __repr__(self):
         return "<User(name='%s', email='%s',password='%s')>" % (self.name, self.email, self.password)
-        #return '<User %r>' % (self.name)
+
 
 class Book(Base):
     __tablename__ = 'books'
     id = Column(Integer, primary_key=True)
     title = Column(String(120))
     author = Column(String(120))
-    curretTime = Column(String(10))
-    user_id = Column(Integer, ForeignKey(User.id))
-    #user = relationship(User, backref=backref('users', lazy='dynamic'))
+    #curretTime = Column(String(10))
+    #user_id = Column(Integer, ForeignKey(User.id))
+    #user = relationship("User" secondary=BookUser, backref='users')
 
-    def __init__(self, title=None, author=None, user_id=None):
+    def __init__(self, title=None, author=None):
         self.title = title
         self.author = author
-        self.user_id = user_id
+        #self.user_id = user_id
 
     def __repr__(self):
-        return "<Book(title='%s', author='%s', user_id='%s')>" % (self.title, self.author,self.user_id)
+        return "<Book(title='%s', author='%s')>" % (self.title, self.author)
 
 class Bookmark(Base):
     __tablename__ = 'bookmarks'
     id = Column(Integer, primary_key=True)
     desc = Column(String(120))
     time = Column(String(20))
-
     book_id = Column(Integer, ForeignKey(Book.id))
     #book = relationship(Book, backref=backref('books', lazy='dynamic'))
-
     user_id = Column(Integer, ForeignKey(User.id))
     #user = relationship(User, backref=backref('users', lazy='dynamic'))
 
@@ -57,18 +56,18 @@ class Bookmark(Base):
         return "<Bookmark(desc='%s', time='%s', book_id='%s', user_id='%s')>" % (self.desc, self.time,self.book_id,self.user_id)
 
 class BookUser(Base):
-    __tablename__ = 'joint'
+    __tablename__ = 'BookUser'
     id = Column(Integer, primary_key=True)
-    currentTime = Column(Integer)
-
+    currentTime = Column(String(10))
     user_id = Column(Integer, ForeignKey(User.id))
-    #user = relationship(User, backref=backref('users', lazy='dynamic'))
-
     book_id = Column(Integer, ForeignKey(Book.id))
+    #user = relationship(User, backref=backref('users', lazy='dynamic'))
     #book = relationship(Book, backref=backref('books', lazy='dynamic'))
 
-    def __init__(self,currentTime = None):
+    def __init__(self,currentTime = None, user_id=None, book_id=None):
         self.currentTime = currentTime
+        self.user_id = user_id
+        self.book_id = book_id
 
     def __repr__(self):
-        return "<Joint(currentTime='%s')>" % (self.currentTime)
+        return "<BookUser(currentTime='%s', user_id='%s',book_id='%s')>" % (self.currentTime,self.user_id,self.book_id)
