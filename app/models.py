5ebc6daf89994d97155262df3c7af54faefe39d4
Aaron <rscaposer@gmail.com>
2016-12-21 00:42:19 -0500

    set default start time to 0.0, next: list books being listened to and make them swithable

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ class BookUser(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
-    currentTime = db.Column(db.Float)
+    currentTime = db.Column(db.Float, default=0.0)
 
 
 class User(db.Model):
